dc468bfb6c7fa7f4da8dfd8f16fa6455ce15e304
jing1324 <jinjing123@gmail.com>
2019-10-25 01:17:39 +0800

    Fix sql tables

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -13,8 +13,7 @@ DROP TABLE IF EXISTS Projects CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
 
 CREATE TABLE Users (
-    user_id serial PRIMARY KEY,
-	email varchar(255) NOT NULL UNIQUE,
+	email varchar(255) NOT NULL PRIMARY KEY,
 	full_name varchar(255) NOT NULL,
     phone_number varchar(255) NOT NULL UNIQUE,
     password_hash varchar(255) NOT NULL
@@ -24,19 +23,19 @@ CREATE TABLE Projects (
     project_name varchar(255) PRIMARY KEY,
     project_description text,
     project_image_url varchar(255),
-    user_id int REFERENCES Users(user_id) ON DELETE CASCADE
+    email varchar(255) REFERENCES Users(email) ON DELETE CASCADE
 );
 
 CREATE TABLE Follows (
-    follower_id int REFERENCES Users(user_id),
-    following_id int REFERENCES Users(user_id),
+    follower_id varchar(255) REFERENCES Users(email),
+    following_id varchar(255) REFERENCES Users(email),
     CONSTRAINT follows_constraint PRIMARY KEY(follower_id, following_id)
 );
 
 CREATE TABLE Likes (
-    user_id int REFERENCES Users(user_id),
+    email varchar(255) REFERENCES Users(email),
     project_name varchar(255) REFERENCES Projects(project_name),
-    CONSTRAINT likes_constraint PRIMARY KEY(user_id, project_name)
+    CONSTRAINT likes_constraint PRIMARY KEY(email, project_name)
 );
 
 CREATE TABLE Transactions (
@@ -45,40 +44,41 @@ CREATE TABLE Transactions (
 );
 
 CREATE TABLE Backs (
-    user_id int REFERENCES Users(user_id),
+    email varchar(255) REFERENCES Users(email),
     project_name varchar(255) REFERENCES Projects(project_name),
     transaction_id int REFERENCES Transactions(transaction_id),
         back_date timestamp NOT NULL,
-    CONSTRAINT backs_constraint PRIMARY KEY(user_id, project_name, transaction_id)
+    CONSTRAINT backs_constraint PRIMARY KEY(email, project_name, transaction_id)
 );
 
 CREATE TABLE Creates (
     project_name varchar(255) REFERENCES Projects(project_name),
-    user_id int REFERENCES Users(user_id),
+    email varchar(255) REFERENCES Users(email),
     create_date timestamp NOT NULL,
-    CONSTRAINT creates_constraint PRIMARY KEY(user_id, project_name)
+    CONSTRAINT creates_constraint PRIMARY KEY(email, project_name)
 );
 
 CREATE TABLE SearchHistory (
     search_timestamp timestamp,
     search_text varchar(255),
-    user_id int REFERENCES Users(user_id),
-    CONSTRAINT searchhistory_timestamp PRIMARY KEY(user_id, search_timestamp)
+    email varchar(255) REFERENCES Users(email),
+    PRIMARY KEY(email,search_timestamp)
 );
 
 CREATE TABLE Searches (
-    search_timestamp timestamp REFERENCES SearchHistory(search_timestamp),
-    user_id int REFERENCES Users(user_id),
-    CONSTRAINT searches_constraint PRIMARY KEY(user_id, search_timestamp)
+    email varchar(255),
+    search_timestamp timestamp,
+    FOREIGN KEY (email,search_timestamp) REFERENCES SearchHistory(email,search_timestamp),
+    PRIMARY KEY (email,search_timestamp)
 );
 
 CREATE TABLE Wallets (
-    user_id int REFERENCES Users(user_id) ON DELETE CASCADE,
+    email varchar(255) REFERENCES Users(email) ON DELETE CASCADE,
     amount numeric NOT NULL
 );
 
 CREATE TABLE Feedback (
-    project_id int REFERENCES Projects(project_id) ON DELETE CASCADE,
+    project_name varchar(255) REFERENCES Projects(project_name) ON DELETE CASCADE,
     comment_text text,
     rating_number int,
 
@@ -88,21 +88,22 @@ CREATE TABLE Feedback (
 );
 
 CREATE TABLE Rewards (
-    project_id int REFERENCES Projects(project_id) ON DELETE CASCADE,
+    project_name varchar(255) REFERENCES Projects(project_name) ON DELETE CASCADE,
     threshold_amount numeric(20,2),
     reward_description text,
-    PRIMARY KEY(project_id, tier_id),
+    tier_id int,
+    PRIMARY KEY(project_name, tier_id),
     CONSTRAINT
       project_tier_constraint
-      UNIQUE(project_id, threshold_amount)
+      UNIQUE(project_name, threshold_amount)
 );
 
 CREATE TABLE Updates (
-    project_id int REFERENCES Projects(project_id) ON DELETE CASCADE,
+    project_name varchar(255) REFERENCES Projects(project_name) ON DELETE CASCADE,
     update_description text,
-    update_date date,
+    update_time timestamp,
 
     CONSTRAINT
       project_update_constraint
-      PRIMARY KEY(update_id, project_id)
+      PRIMARY KEY(update_time, project_name)
 );
