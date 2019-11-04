acba182d91bb0835c012c059344ea702685e5389
jing1324 <jinjing123@gmail.com>
2019-11-05 00:48:23 +0800

    Moved timestamp from frontend to psql

--- routes/search.js
+++ routes/search.js
@@ -17,9 +17,8 @@ const pool = new Pool({
 router.post("/", function(req, res, next) {
   console.log("at login");
   var email = req.body.email;
-  var timestamp = req.body.timestamp;
   var search_text = req.body.search_text;
-  const query = `CALL search('${email}','${timestamp}','${search_text}')`;
+  const query = `CALL search('${email}','${search_text}')`;
   pool.query(query, (error, data) => {
     if (error) {
       res.send(error);
