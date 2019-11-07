89b93731818b9676ac911d46bdb7833fc96bde35
jing1324 <jinjing123@gmail.com>
2019-11-07 23:36:40 +0800

    Show complex query results in frontend

--- routes/search.js
+++ routes/search.js
@@ -14,7 +14,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
 
-router.post("/", function(req, res, next) {
+router.post("/add", function(req, res, next) {
   var email = req.body.email;
   var search_text = req.body.search_text;
   const query = `CALL search('${email}','${search_text}')`;
@@ -27,7 +27,7 @@ router.post("/", function(req, res, next) {
   });
 });
 
-router.get("/:email", function(req, res, next) {
+router.get("/history/:email", function(req, res, next) {
   const query = `SELECT * FROM SearchHistory NATURAL JOIN SEARCHES WHERE email='${req.params.email}';`;
   pool.query(query, (error, data) => {
     if (error) {
@@ -46,6 +46,41 @@ router.post("/clear", function(req, res, next) {
       res.send(error);
     } else {
       res.send("success");
+    }
+  });
+});
+
+router.get("/project/:searchtext", function(req, res, next) {
+  var search_text = req.params.searchtext;
+  const query = `SELECT * FROM projects WHERE project_name LIKE '%${search_text}%';`;
+  pool.query(query, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data.rows);
+    }
+  });
+});
+
+router.get("/user/:searchtext", function(req, res, next) {
+  var search_text = req.params.searchtext;
+  const query = `SELECT * FROM users WHERE email LIKE '%${search_text}%';`;
+  pool.query(query, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data.rows);
+    }
+  });
+});
+
+router.get("/popsearches", function(req, res, next) {
+  const query = `SELECT search_text, COUNT(*) FROM SearchHistory GROUP BY search_text ORDER BY COUNT(*) DESC LIMIT 2;`;
+  pool.query(query, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data.rows);
     }
   });
 });
